ae8bd52bfcd99f777013e9bc16b33771927a428f
David Krutsko <dave@krutsko.net>
2013-11-06 18:20:48 -0500

    Initial version completed
    > Added documentation

--- README.md
+++ README.md
@@ -1,6 +1,66 @@
 # Express Bouncer
+[![Build Status](https://travis-ci.org/dkrutsko/Express-Bouncer.png)](https://travis-ci.org/dkrutsko/Express-Bouncer)
+[![NPM version](https://badge.fury.io/js/express-bouncer.png)](https://badge.fury.io/js/express-bouncer)
 
+A simple and standalone middleware for [express](https://github.com/visionmedia/express) routes which attempts to mitigate brute-force attacks. It works by increasing the delay with each failed request using a Fibonacci formula. Requests are tracking via IP address and can be white-listed or reset on demand. All logged addresses are stored locally in an object and dormant addresses are removed automatically. Error messages are also completely customizable. This project is based on [express-brute](https://github.com/AdamPflug/express-brute) created by [Adam Pflug](https://github.com/AdamPflug).
 
+### Installation
+```js
+$ npm install express-bouncer
+```
+
+### Quick Start
+```js
+// Creates a new instance of our bouncer (args optional)
+var bouncer = require ("express-bouncer")(500, 900000);
+
+// Add white-listed addresses (optional)
+bouncer.whitelist.push ("127.0.0.1");
+
+// In case we want to supply our own error (optional)
+bouncer.blocked = function (req, res, next, remaining)
+{
+	res.send (403, "Too many requests have been made, " +
+		"please wait " + remaining / 1000 + " seconds");
+};
+
+// Route we wish to protect with bouncer middleware
+app.post ("/login", bouncer.block, function (req, res)
+{
+	if (LoginFailed)
+	{
+		// Login failed
+	}
+
+	else
+	{
+		bouncer.reset (req);
+		// Login succeeded
+	}
+});
+
+// Clear all logged addresses
+// (Usually never really used)
+bouncer.addresses = { };
+```
+
+### Documentation
+#### Constructor
+```js
+express-bouncer ([min], [max], [free])
+```
+* **min** The minimum number of milliseconds the user can be forced to wait. *(default: 500 ms)*
+* **max** The maximum number of milliseconds the user can be forced to wait. *(default: 10 min)*
+* **free** The number of attempts a user can make before being forced to wait. *(default: 2)*
+
+#### Functions
+* **reset** Resets the wait time between attempts for the specified request.
+* **block** Middleware that will block requests which are occurring too often.
+
+#### Properties
+* **addresses** A list of logged IP addresses. Cleared by overriding with new object.
+* **whitelist** A list of white-listed IP addresses. These addresses will never be blocked.
+* **blocked** Function to be called when a request has been blocked. *(see quick start)*
 
 ### Author
 * Email: <dave@krutsko.net>
--- index.js
+++ index.js
@@ -14,40 +14,81 @@
 ////////////////////////////////////////////////////////////////////////////////
 /// Represents the main bouncer application exposed through the exports module.
 
-function bouncer()
+function bouncer (min, max, free)
 {
+	//----------------------------------------------------------------------------//
+	// Constructor                                                                //
+	//----------------------------------------------------------------------------//
+
+	// Bind this instance
+	var instance = this;
+
+	// Setup any required default arguments
+	if (typeof min  !== "number") min  = 500;
+	if (typeof max  !== "number") max  = 600000;
+	if (typeof free !== "number") free = 2;
+
+	// Validate the arguments
+	if (min  <   1) min  = 1;
+	if (max  < min) max  = min;
+	if (free <   0) free = 0;
+
+	// Store precomputed delays
+	var delays = [ ];
+
+	// Include any free attempts
+	while (free--) delays.push (0);
+
+	delays.push (min);
+	delays.push (min);
+	while (true)
+	{
+		// Compute delays using Fibonacci formula
+		var value = delays[delays.length - 1] +
+					delays[delays.length - 2];
+
+		if (value > max) {
+			delays.push (max);
+			break;
+		}
+		
+		delays.push (value);
+	}
+
+	setInterval (function() {
+		var now = Date.now();
+		// Remove any possible dormant addresses
+		for (var address in instance.addresses) {
+			if (now - instance.
+				addresses[address].lastAttempt > max)
+				delete instance.addresses[address];
+		}
+	}, 1800000);
+
+
+
 	//----------------------------------------------------------------------------//
 	// Properties                                                                 //
 	//----------------------------------------------------------------------------//
 
 	////////////////////////////////////////////////////////////////////////////////
-	/// A list of current failed attempts. Clear by overriding with new object.
+	/// A list of logged IP addresses. Cleared by overriding with new object.
 
-	this.failures = { };
+	this.addresses = { };
 
 	////////////////////////////////////////////////////////////////////////////////
-	/// A list of white-listed ip addresses. These addresses will not be blocked.
+	/// A list of white-listed IP addresses. These addresses will never be blocked.
 
 	this.whitelist = [ ];
 
 	////////////////////////////////////////////////////////////////////////////////
-	/// The number of attempts the user can make before being forced to wait.
-
-	this.freeAttempts = 2;
-
-	////////////////////////////////////////////////////////////////////////////////
-	/// The minimum and maximum milliseconds the user will be forced to wait.
-
-	this.minWait = 500;
-	this.maxWait = 600000;
-
-	////////////////////////////////////////////////////////////////////////////////
 	/// Called when a request has been blocked. Override for custom functionality.
-	/// remaining: milliseconds remaining before a new attempt can be made.
 
-	this.failure = function (req, res, next, remaining)
+	this.blocked = function (req, res, next, remaining)
 	{
-	}
+		res.send (403, "Too many requests have been made, " +
+			"please wait " + remaining / 1000 + " seconds");
+	};
 
 
 
@@ -60,14 +101,53 @@ function bouncer()
 
 	this.reset = function (req)
 	{
-	}
+		var address =
+			req.headers["x-forwarded-for"] || req.connection.remoteAddress ||
+			req.socket.remoteAddress || req.connection.socket.remoteAddress;
+
+		// Remove the current address from block list
+		address && delete instance.addresses[address];
+	};
 
 	////////////////////////////////////////////////////////////////////////////////
-	/// Middleware which will block requests that are happening too often.
+	/// Middleware that will block requests which are occurring too often.
 
 	this.block = function (req, res, next)
 	{
-	}
+		var address =
+			req.headers["x-forwarded-for"] || req.connection.remoteAddress ||
+			req.socket.remoteAddress || req.connection.socket.remoteAddress;
+
+		// Allow any invalid or white-listed IP addresses
+		if (!address || instance.whitelist.indexOf (address) > -1)
+			{ typeof next === "function" && next(); return; }
+
+		// Retrieve or create logged address
+		var fail = instance.addresses[address] ||
+					{ count: 0, lastAttempt: 0 };
+
+		// Compute the wait time remaining
+		var remaining = fail.lastAttempt +
+			delays[fail.count] - Date.now();
+			
+		if (remaining > 0)
+			// Stop current request from going through
+			instance.blocked (req, res, next, remaining);
+
+		else
+		{
+			// Increment the request counter
+			fail.lastAttempt = Date.now();
+			if (fail.count < delays.length - 1)
+				fail.count++;
+
+			// Save the address back to the list
+			instance.addresses[address] = fail;
+
+			// Allow the request to go through
+			typeof next === "function" && next();
+		}
+	};
 }
 
 
@@ -76,4 +156,5 @@ function bouncer()
 // Exports                                                                    //
 //----------------------------------------------------------------------------//
 
-exports = module.exports = function() { return new bouncer() };
+exports = module.exports =
+	function (min, max, free) { return new bouncer (min, max, free); };
